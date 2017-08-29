9ae9948d3fb67e897639a4953c1f079cdfd03f52
IgorSavchenko <ig0r@ua.fm>
2017-08-29 13:01:12 +0300

    functions added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,24 @@
 //----------------------------------------
-function hide(element) {
+function hideAll(element) {
   for (i = 0; i < element.length; i++) {
     element[i].style.display = 'none';
   }
+}
+function hide(e) {
+  e.style.display = 'none';
+}
+function show(e) {
+  e.style.display = 'block';
 }
 //----------------------------------------
 document.addEventListener("DOMContentLoaded", () => {
   let content = Array.from(document.querySelectorAll('div.fb-section__content'));
   let sections = Array.from(document.querySelectorAll('.fb-section'));
-  hide(content);
+  setTimeout(hideAll(content), 1000);
   for (let i = 0; i < sections.length; i++) {
     sections[i].addEventListener('click', () => {
-      if (content[i].style.display == 'none') content[i].style.display = 'block';
-      else content[i].style.display = 'none';
+      if (content[i].style.display == 'none') setTimeout(show(content[i]), 1000);
+      else setTimeout(hide(content[i]), 1000);
     });
   }
 });
