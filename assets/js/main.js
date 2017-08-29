b655c413a703e90ee4d064e06b4bf9361c8758b5
IgorSavchenko <ig0r@ua.fm>
2017-08-29 11:49:28 +0300

    Add JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-
+//----------------------------------------
+function hide(element) {
+  for (i = 0; i < element.length; i++) {
+    element[i].style.display = 'none';
+  }
+}
+//----------------------------------------
+document.addEventListener("DOMContentLoaded", () => {
+  let content = Array.from(document.querySelectorAll('div.fb-section__content'));
+  let sections = Array.from(document.querySelectorAll('.fb-section'));
+  hide(content);
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].addEventListener('click', () => {
+      if (content[i].style.display == 'none') content[i].style.display = 'block';
+      else content[i].style.display = 'none';
+    });
+  }
+});
